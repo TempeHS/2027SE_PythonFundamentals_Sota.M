56befe3cf38d4f77b1fc7f8f326407fe9fd4ae8d
Sota M <sota.ma@education.nsw.gov.au>
2026-02-17 02:59:32 +0000

    Upload completed bank.py

---
+++ 1-Conditionals/bank/bank.py
@@ -0,0 +1,8 @@
+greeting = input("Greeting: ")
+
+if greeting.startswith("Hello") or greeting.startswith("Hello"):
+    print("$0")
+elif greeting.startswith("H") or greeting.startswith("h"):
+    print("$20")
+else:
+    print("$100")
